f1acdcf7377c797eba757554db57b4d4277c3f8a
liuqin <1053544563@qq.com>
2019-10-29 23:56:26 +0800

    添加一些公共方法

---
+++ public/static/admin/js/apiform.js
@@ -0,0 +1,25 @@
+dui.define('apiform',['jquery','form','popup'],function($,form,popup){
+    var thisPopup,
+    SELECTOR={
+
+    },
+    ApiForm = {
+        render:function(el){
+            form.render(el);
+            // 初始化tabs
+
+
+
+
+        }
+    },
+    Tabs = function(el){
+        
+
+
+
+
+
+    };
+    return ApiForm;
+})
